f38658d9e053cf1e73cbeb6a1ba27be0bdc74cdb
Peter Sheehan <ak22112@bristol.ac.uk>
2024-03-16 21:38:05 +0000

    [Documentation] adds comments to ARViewWrapper

--- AIAR/AIAR/AR/ARViewWrapper.swift
+++ AIAR/AIAR/AR/ARViewWrapper.swift
@@ -8,14 +8,26 @@
 import RealityKit
 import ARKit
 
+/// A class responsible for managing the AR view and handling the loading of 3D models into the AR scene.
+///
+/// This class wraps around an instance of `ARView`, hence it is called `ARViewWrapper`.
 class ARViewWrapper: ObservableObject {
+    
+    /// The ARView instance used to display the augmented reality scene.
     let arView: ARView
+    
+    /// The detected image anchor used as a reference for placing 3D models in the AR scene.
     var detectedImageAnchor: ARImageAnchor?
     
+    /// Initialises the ARViewWrapper and creates an ARView instance.
     init() {
         self.arView = ARView(frame: .zero)
     }
     
+    /// Loads a 3D model into the AR scene at the location of the detected QR code image anchor.
+    /// - Parameters:
+    ///   - path: The path to the USDZ file containing the 3D model.
+    ///   - imageAnchor: The ARImageAnchor representing the detected QR code image.
     func loadModel(path: String, imageAnchor: ARImageAnchor) {
         
         // Download the usdz file from Firebase Storage
@@ -47,16 +59,15 @@ class ARViewWrapper: ObservableObject {
                 }
             }
         }
-        
     }
     
+    /// Resets the AR view by removing all existing anchors and restarting the AR session with new reference images.
+    /// - Parameter referenceImages: The set of AR reference images used for image detection.
     func resetARView(withReferenceImages referenceImages: Set<ARReferenceImage>) {
-        
         self.arView.session.pause()
         self.arView.scene.anchors.removeAll()
         let configuration = ARWorldTrackingConfiguration()
         configuration.detectionImages = referenceImages
         self.arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-        
     }
 }
