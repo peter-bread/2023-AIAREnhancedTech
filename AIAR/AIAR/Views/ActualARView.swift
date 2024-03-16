06dcaf881b6612a65a6c632dccb3a47fc844612d
Peter Sheehan <ak22112@bristol.ac.uk>
2024-03-16 21:58:48 +0000

    [Documentation] adds comments to `ActualARView`

--- AIAR/AIAR/Views/ActualARView.swift
+++ AIAR/AIAR/Views/ActualARView.swift
@@ -8,18 +8,23 @@
 import SwiftUI
 import ARKit
 
+/// A SwiftUI view representing the actual AR experience, incorporating an AR view 
+/// with image detection and a reset button overlay.
 struct ActualARView: View {
-    /// Set of `ARReferenceImage` that will be used to detect images in the real world
+    
+    /// Set of `ARReferenceImage` that will be used to detect images in the real world.
     var referenceImages: Set<ARReferenceImage>
     
+    /// A binding to determine whether the AR view should be reset.
     @Binding var shouldReset: Bool
     
     var body: some View {
+        
+        // Embeds the `ARViewRepresentable` within the SwiftUI view hierarchy, providing AR functionality.
         ARViewRepresentable(referenceImages: referenceImages, shouldReset: $shouldReset)
             .ignoresSafeArea()
         
-            // overlays (for now) one button on the ARiew inside ARViewContainer so
-            // the user can reset the ARView whenever it stops working.
+            // Adds a button overlay to reset the AR view when necessary.
             .overlay(alignment: .bottom, content: {
                 Button(action: {
                     self.shouldReset = true
