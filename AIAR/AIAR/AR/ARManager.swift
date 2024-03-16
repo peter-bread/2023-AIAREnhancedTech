3c361ea81ab02454d832aead68e5de31dd37eba4
Peter Sheehan <ak22112@bristol.ac.uk>
2024-03-16 21:39:23 +0000

    [Documentation] adds comments to ARManager

--- AIAR/AIAR/AR/ARManager.swift
+++ AIAR/AIAR/AR/ARManager.swift
@@ -7,7 +7,13 @@
 
 import Combine
 
+/// A singleton class responsible for managing AR session actions and
+/// facilitating communication between different components of the AR application.
 class ARManager {
+    /// The shared instance of `ARManager`, providing a single point of access
+    /// throughout the application.
     static let shared = ARManager()
+    
+    /// A `PassthroughSubject` used for publishing AR session actions to subscribers.
     let actionStream = PassthroughSubject<ARAction, Never>()
 }
