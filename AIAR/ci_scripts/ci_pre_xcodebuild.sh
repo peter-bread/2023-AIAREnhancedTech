8cb9fbaf81fa24bf1ededeb1d79089a623f0d3fd
Peter Sheehan <ak22112@bristol.ac.uk>
2024-03-18 14:03:45 +0000

    change google...plist location

--- AIAR/ci_scripts/ci_pre_xcodebuild.sh
+++ AIAR/ci_scripts/ci_pre_xcodebuild.sh
@@ -16,10 +16,8 @@ convert() {
 	esac
 }
 
-pwd
-
 # Create and populate GoogleService-Info.plist
-cat <<EOF >GoogleService-Info
+cat <<EOF >../AIAR/GoogleService-Info.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
