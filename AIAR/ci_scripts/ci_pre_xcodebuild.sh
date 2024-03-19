375561287f381526f9c2cfc2b518122ca913e36f
Peter Sheehan <ak22112@bristol.ac.uk>
2024-03-19 00:06:15 +0000

    add safeguards to ci script to prevent it overwriting the `.plist` file in a local environment

--- AIAR/ci_scripts/ci_pre_xcodebuild.sh
+++ AIAR/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# THIS SCRIPT IS FOR CI WORKFLOWS ON XCODE CLOUD ONLY
+# DO NOT RUN IN LOCAL ENVIRONMENT
+
 # convert YES or NO to <true></true> or <false></false>
 convert() {
 	case $1 in
@@ -10,14 +13,25 @@ convert() {
 		echo "<false></false>"
 		;;
 	*)
-		echo "Error: Invalid value '$1'. Must be 'yes' or 'no'." >&2
+		echo "Error: Invalid value '$1'. Must be 'YES' or 'NO'." >&2
 		exit 1
 		;;
 	esac
 }
 
+# Path to where GoogleService-Info.plist should be
+PLIST_FILEPATH="../AIAR/GoogleService-Info.plist"
+
+# Check if GoogleService-Info.plist already exists
+# If it exists, do not overwrite it
+# This is a safeguard to protect against running in a local environment where the environment variables haven't been set
+if [ -e "$PLIST_FILEPATH" ]; then
+	echo "Warning: File '$PLIST_FILEPATH' already exists. Skipping creation." >&2
+	exit 0
+fi
+
 # Create and populate GoogleService-Info.plist
-cat <<EOF >../AIAR/GoogleService-Info.plist
+cat <<EOF >"$PLIST_FILEPATH"
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
